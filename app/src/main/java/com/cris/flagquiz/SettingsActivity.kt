4291aed5c30a834a46bc2ca33ce4c1f616292c13
Cris <crisuvas@gmail.com>
2018-04-29 15:40:05 -0500

    commit

--- app/src/main/java/com/cris/flagquiz/SettingsActivity.kt
+++ app/src/main/java/com/cris/flagquiz/SettingsActivity.kt
@@ -1,7 +1,6 @@
 package com.cris.flagquiz
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 
 import kotlinx.android.synthetic.main.activity_settings.*
@@ -13,10 +12,6 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
